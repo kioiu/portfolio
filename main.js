ee334fa206ea8aa98b6f58a00bdbc3615049e847
kioiu <otaku0215.a@gmaill.com>
2026-05-17 21:32:40 +0900

    https://github.com/kioiu/portfolio.git

---
+++ main.js
@@ -0,0 +1,45 @@
+const images = [
+  "images/dolphin.JPG",
+
+  "images/night.jpg",
+
+  "images/sunset.JPG",
+
+  "images/blueworld.JPG",
+
+  "images/hotaru.jpg",
+
+  "images/sigma.png",
+
+  "images/sunset2.jpg",
+
+  "images/tokei.jpg"
+];
+
+const heroBg = document.querySelector(".hero-bg");
+
+let currentIndex = 0;
+
+function changeHeroImage() {
+
+  heroBg.style.opacity = 0;
+
+  setTimeout(() => {
+
+    heroBg.style.backgroundImage =
+      `url(${images[currentIndex]})`;
+
+    heroBg.style.opacity = 0.35;
+
+    currentIndex++;
+
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+
+  }, 1000);
+}
+
+changeHeroImage();
+
+setInterval(changeHeroImage, 5000);
